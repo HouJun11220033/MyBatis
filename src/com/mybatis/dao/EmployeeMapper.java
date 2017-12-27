5998051e1fc7f051864c226e6165701963d26d23
侯鋆 <1144724015@qq.com>
2017-12-27 20:30:08 +0800

    完成第一次查询

---
+++ src/com/mybatis/dao/EmployeeMapper.java
@@ -0,0 +1,7 @@
+package com.mybatis.dao;
+
+import com.mybatis.bean.Employee;
+
+public interface EmployeeMapper {
+	public Employee getEmpById(Integer id);
+}
--- src/com/mybatis/test/HelloMyBatis.java
+++ src/com/mybatis/test/HelloMyBatis.java
@@ -1,9 +1,34 @@
 package com.mybatis.test;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import com.mybatis.bean.Employee;
+
 public class HelloMyBatis {
 
-	public HelloMyBatis() {
-		
+	public SqlSessionFactory getSqlSessionFactory() throws IOException{
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		return new SqlSessionFactoryBuilder().build(inputStream);
+	}
+	
+	@Test
+	public void test() throws IOException{
+		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+		SqlSession openSession = sqlSessionFactory.openSession();
+		try {
+			Employee employee = openSession.selectOne("com.mybatis.dao.EmployeeMapper.getEmpById", 1);
+			System.out.println(employee);
+		} finally {
+			openSession.close();
+		}
 	}
 
 }
